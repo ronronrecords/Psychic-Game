d34529d5e6a2de1a7cd67207d0b59fd5ed4ef7b9
ronronrecords <info@ronronrecords.com>
2017-10-27 11:16:08 -0700

    With New Button script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,14 @@
-
-
 var letters = ["u", "c", "i",]
 
-for (i = 0; i < 4; i++);
+var rand = letters[Math.floor(Math.random() * letters.length)];
 
+function promptMe(){
+    var userAdjective = prompt("Please provide a Letter");
+    if (userAdjective == randGen);
+    alert ("Your Abilities Amaze Me");
+}
+
+function randGen(){
+    var rand = letters[Math.floor(Math.random() * letters.length)];
+    console.log();
 }
